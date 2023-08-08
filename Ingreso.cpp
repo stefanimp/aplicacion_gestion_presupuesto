09db60b43ff3a7d427e498e0f54e906f9c86035f
stefanimp <stefan408.imperato@gmail.com>
2023-08-08 18:37:46 +0200

    Declaración e implementación de la clase Ingreso.

---
+++ Ingreso.cpp
@@ -0,0 +1,16 @@
+//
+// Created by stefan on 08/08/2023.
+//
+#include "Ingreso.h"
+
+Ingreso::Ingreso(std::string nombre, std::string descripcion): Categoria(nombre, descripcion) {
+
+}
+
+Ingreso::~Ingreso() noexcept {
+
+}
+
+std::string Ingreso::getNombre() const {
+    return "Ingreso:" + Categoria::getNombre();
+}
---
+++ Ingreso.h
@@ -0,0 +1,20 @@
+//
+// Created by stefan on 08/08/2023.
+//
+
+#ifndef CONTROL_DE_GASTOS_INGRESO_H
+#define CONTROL_DE_GASTOS_INGRESO_H
+
+#include "Categoria.h"
+
+class Ingreso:public Categoria{
+public:
+    Ingreso() = default;
+    Ingreso(std::string nombre, std::string descripcion);
+    Ingreso(const Ingreso &original);
+    virtual ~Ingreso();
+    //Getters y settres
+    virtual std::string getNombre() const override;
+};
+
+#endif //CONTROL_DE_GASTOS_INGRESO_H
