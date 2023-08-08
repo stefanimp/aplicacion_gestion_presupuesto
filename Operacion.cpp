c1e48a3803e52cc0059f1619a890201f0e59fe54
stefanimp <stefan408.imperato@gmail.com>
2023-08-08 18:15:34 +0200

    Primera implementación de la clase Operacion

---
+++ Operacion.cpp
@@ -0,0 +1,53 @@
+//
+// Created by stefan on 08/08/2023.
+//
+#include "Operacion.h"
+
+Operacion::Operacion(std::string nombre, float cantidad_monetaria, std::string descripcion, Categoria *tipo):nombre(nombre), descripcion(descripcion),cantidad_monetaria(cantidad_monetaria), tipo(tipo) {
+    try{
+        if(nombre == ""){
+            throw std::invalid_argument("Operacion::Operacion, Error, el nombre de la operacion no puede estar vacio.");
+        }
+        if(descripcion == ""){
+            throw std::invalid_argument("Operacion::Operacion, Error, el nombre de la operacion no puede estar vacio");
+        }
+    }catch (std::invalid_argument &e){
+        std::cout<<e.what() <<std::endl;
+    }
+}
+
+Operacion::~Operacion() {
+
+}
+
+void Operacion::setNombre(std::string nombre) {
+    this->nombre = nombre;
+}
+
+void Operacion::setDescripcion(std::string descripcion) {
+    this->descripcion = descripcion;
+}
+
+void Operacion::setCantMonetaria(float cantidad_monetaria) {
+    this->cantidad_monetaria = cantidad_monetaria;
+}
+
+void Operacion::setTipo(Categoria *tipo) {
+    this->tipo = tipo;
+}
+
+std::string Operacion::getNombre() const {
+    return nombre;
+}
+
+std::string Operacion::getDescripcion() const {
+    return descripcion;
+}
+
+float Operacion::getCantMonetaria() const {
+    return cantidad_monetaria;
+}
+
+Categoria *Operacion::getTipo() const {
+    return tipo;
+}
--- Operacion.h
+++ Operacion.h
@@ -6,15 +6,28 @@
 #define CONTROL_DE_GASTOS_OPERACION_H
 
 #include "Categoria.h"
-#include "Fecha.h"
 
 class Operacion{
 private:
     std::string nombre = "";
-    float cantidad_monetaria = 0;   //POr defecto ene euros
+    float cantidad_monetaria = 0;   //Por defecto ene euros
     std::string descripcion = "";
-    Fecha fecha;
     Categoria *tipo = nullptr;
+public:
+    //Constructores y destructores
+    Operacion() = default;
+    Operacion(std::string nombre, float cantidad_monetaria, std::string descripcion, Categoria *tipo);
+    Operacion(const Operacion &original);
+    ~Operacion();
+    //Getters y setters
+    void setNombre(std::string nombre);
+    void setCantMonetaria(float cantidad_monetaria);
+    void setDescripcion(std::string descripcion);
+    void setTipo(Categoria *tipo);
+    std::string getNombre() const;
+    float getCantMonetaria() const;
+    std::string getDescripcion() const;
+    Categoria* getTipo() const;
 
 };
 
